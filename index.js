471af68c0b0a0bdffb6de043c9d663e2267b808c
KoliosMarios <koliosmarios@gmail.com>
2022-09-05 18:49:15 +0300

    making squares playable or not

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const squares = document.querySelectorAll(".board div");
 const result = document.querySelector("#result");
 
 // pick out the current player display
-const displayCurrentPlayer = document.querySelector("#current player");
+const displayCurrentPlayer = document.querySelector("#current-player");
 
 // current player variable to display
 let currentPlayer = 1;
@@ -13,6 +13,23 @@ let currentPlayer = 1;
 // add an onClick on all our squares
 for (let i = 0; i < squares.length; i++) {
   squares[i].onclick = () => {
-    alert("You have clicked square " + i);
+    // we alert the player if he tries to play a taken square
+    if (squares[i].classList.contains("taken")) {
+      return;
+    }
+    // we make a square playable only if the square below it is taken
+    else if (squares[i + 7].classList.contains("taken")) {
+      if (currentPlayer == 1) {
+        squares[i].classList.add("taken");
+        squares[i].classList.add("player-one");
+        currentPlayer = 2;
+        displayCurrentPlayer.innerHTML = currentPlayer;
+      } else if (currentPlayer == 2) {
+        squares[i].classList.add("taken");
+        squares[i].classList.add("player-two");
+        currentPlayer = 1;
+        displayCurrentPlayer.innerHTML = currentPlayer;
+      }
+    } else alert("Non Playable!");
   };
 }
